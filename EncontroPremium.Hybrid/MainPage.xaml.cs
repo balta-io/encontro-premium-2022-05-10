1a691616a31a7a09105c8e9003d99069bfb567d7
Andre Baltieri <andrebaltieri@hotmail.com>
2022-05-10 12:21:21 -0300

    Adicionado suporte a MAUI e Blazor Hybrid Apps

---
+++ EncontroPremium.Hybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EncontroPremium.Hybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EncontroPremium.Mobile/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace EncontroPremium.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        string count = string.Empty;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count = Core.PasswordGenerator.Generate();
+            CounterLabel.Text = $"Current count: {count}";
+
+            SemanticScreenReader.Announce(CounterLabel.Text);
+        }
+    }
+}
